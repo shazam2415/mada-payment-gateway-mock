dc255a59c1cc0a3ff6575437b2efea3f74fa4139
shazam2415 <kocisa513@gmail.com>
2026-03-05 22:03:26 +0300

    added docker integration and some other features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,79 @@
+import { useState } from 'react';
 import axios from 'axios';
+import './App.css';
 
 function App() {
-    const handlePay = async () => {
-        try {
-            const res = await axios.post('http://localhost:5000/api/initiate-payment', {
-                amount: 100,
-                currency: 'SAR'
-            });
-            alert("Payment initiated! Redirected ID: " + res.data.id);
-        } catch (err) {
-            console.error("Hata:", err);
-        }
-    };
+  const [step, setStep] = useState(1); // 1 ile başlıyoruz
+  const [cardNo, setCardNo] = useState('');
+  const [smsCode, setSmsCode] = useState('');
 
-    return (
+  const sendCard = async () => {
+
+    if (cardNo.length !== 16) {
+    alert("Hata: mada kart numarası tam 16 hane olmalıdır!");
+    return; // 'return' burada "aşağıdaki kodlara geçme, fonksiyonu burada bitir" demek.
+  }
+
+  // 2. Bekçi Kontrolü: Sadece rakam mı girilmiş? (Opsiyonel meraklısı için)
+  if (isNaN(cardNo)) {
+    alert("Hata: Kart numarası sadece rakamlardan oluşmalıdır!");
+    return;
+  }
+
+    try {
+      const res = await axios.post('http://localhost:5000/api/initiate-payment', { card: cardNo });
+      
+      // Eğer backend "onay_bekliyor" dediyse adımı 2 yap
+      if (res.data.state === "waiting_for_approval") {
+        setStep(2); 
+      }
+    } catch (err) {
+      alert("Error: " + err.response.data.message);
+    }
+  };
+
+  const verifyCode = async () => {
+    try {
+        const res = await axios.post('http://localhost:5000/api/verify-code', { 
+            code: smsCode
+        });
+
+        alert(res.data.message);
+        setStep(3); 
+    } catch (err) {
+        alert("ERROR: " + err.response.data.message);
+    }
+};
+
+  return (
+    <div className='main' style={{ padding: '30px' }}>
+      <h1>Mada Payment System</h1>
+
+      {/* ADIM 1: KART GİRİŞİ */}
+      {step === 1 && (
         <div>
-            <h1>
-                MADA PAYMENT TEST
-            </h1>
-            <button onClick={handlePay}>
-                Pay 100 SAR
-            </button>
+          <input 
+            placeholder="Card Number" 
+            onChange={(e) => setCardNo(e.target.value)} 
+          />
+          <p>Card no: {cardNo}</p>
+          <button onClick={sendCard}>Continue</button>
         </div>
-    );
+      )}
+
+      {/* ADIM 2: SMS KODU GİRİŞİ */}
+      {step === 2 && (
+        <div>
+          <p>Enter the code sent to your phone:</p>
+          <input 
+            placeholder="SMS Code" 
+            onChange={(e) => setSmsCode(e.target.value)} 
+          />
+          <button className='verify' onClick={verifyCode}>Verify</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -7,15 +7,28 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/api/initiate-payment', (req, res) => {
-    const { amount, currency } = req.body;
 
-    console.log(`payment process started for ${amount} ${currency}.`)
+    const cardNumber = req.body.card;
 
-    res.json({
-        id: "tr_123456",
-        checkout_url: "https://mock-data-gateway.com/pay/123456",
-        status: "initiated"
-    });
+    if (cardNumber.startsWith('4') || cardNumber.startsWith('5')) {
+        res.json({
+            state: "waiting_for_approval",
+            message: "Please enter the 4-digit code sent to your phone.",
+            test_code: "1234"
+        });
+    } else {
+        res.status(400).json({ message: "Invalid mada card!" });
+    }
+});
+
+app.post('/api/verify-code', (req, res) => {
+    const codeReceived = req.body.code;
+
+    if (codeReceived === "1234") {
+        res.json({ message: "Payment Completed Successfully! Approval received from mada network." });
+    } else {
+        res.status(400).json({ message: "Invalid SMS code! Please try again." });
+    }
 });
 
 const PORT = 5000;
